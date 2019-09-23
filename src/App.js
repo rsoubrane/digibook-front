ee4513effc435778a43c2c76bef919ded8015dc1
Romain <romain@romains-macbook-pro.home>
2019-09-23 16:23:00 +0200

    Fix Login and Signup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 
+import jwtDecode from "jwt-decode";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 
 // Components
 import Navbar from "./components/Navbar";
+import themeObject from "./utils/theme";
+import AuthRoute from "./utils/AuthRoute";
 
 //Pages
 import home from "./pages/home";
@@ -14,43 +17,39 @@ import signup from "./pages/signup";
 
 import "./App.css";
 
-const theme = createMuiTheme({
-	palette: {
-		primary: {
-			light: "#33c9dc",
-			main: "#00bcd4",
-			dark: "#008394",
-			contrastText: "#fff"
-		},
-		secondary: {
-			light: "#ff6333",
-			main: "#ff3d00",
-			dark: "#b22a00",
-			contrastText: "#fff"
-		}
-	},
-	typography: {
-		useNextVariants: true
-	}
-});
+const theme = createMuiTheme(themeObject);
 
-function App() {
-	return (
-		<MuiThemeProvider theme={theme}>
-			<div className='App'>
-				<Router>
-					<Navbar />
-					<div className='container'>
-						<Switch>
-							<Route exact path='/' component={home} />
-							<Route exact path='/login' component={login} />
-							<Route exact path='/signup' component={signup} />
-						</Switch>
-					</div>
-				</Router>
-			</div>
-		</MuiThemeProvider>
-	);
+let authenticated;
+const token = localStorage.FBIdToken;
+if (token) {
+	const decodedToken = jwtDecode(token);
+	if (decodedToken.exp * 1000 < Date.now()) {
+		authenticated = false;
+		window.location.href = "/login";
+	} else {
+		authenticated = true;
+	}
+}
+
+class App extends Component {
+	render() {
+		return (
+			<MuiThemeProvider theme={theme}>
+				<div className='App'>
+					<Router>
+						<Navbar />
+						<div className='container'>
+							<Switch>
+								<Route exact path='/' component={home} />
+								<AuthRoute exact path='/login' component={login} authenticated={authenticated} />
+								<AuthRoute exact path='/signup' component={signup} authenticated={authenticated} />
+							</Switch>
+						</div>
+					</Router>
+				</div>
+			</MuiThemeProvider>
+		);
+	}
 }
 
 export default App;
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -13,37 +13,9 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
-const styles = {
-	form: {
-		textAlign: "center"
-	},
-	image: {
-		margin: "20px auto 20px auto"
-	},
-	pageTitle: {
-		margin: "10px auto 10px auto"
-	},
-	textField: {
-		margin: "10px auto 10px auto"
-	},
-	button: {
-		marginTop: 20,
-		position: "relative"
-	},
-	customError: {
-		color: "red",
-		fontSize: "0.8rem",
-		marginTop: 10
-	},
-	progress: {
-		position: "absolute"
-	},
-	signup: {
-		position: "relative",
-		margin: "20px",
-		padding: "20px"
-	}
-};
+const styles = theme => ({
+	...theme.usersControl
+});
 
 class login extends Component {
 	constructor() {
@@ -72,6 +44,7 @@ class login extends Component {
 				this.setState({
 					loading: false
 				});
+				localStorage.setItem("FBIdToken", `Bearer ${res.data.token}`);
 				this.props.history.push("/");
 			})
 			.catch(err => {
@@ -83,10 +56,14 @@ class login extends Component {
 	};
 
 	handleChange = event => {
+        let errors = this.state.errors.filter(item => item !== event.target.value)
+
 		this.setState({
-			[event.target.name]: event.target.value
+			[event.target.name]: event.target.value,
+			errors
 		});
-	};
+    };
+
 
 	render() {
 		const { classes } = this.props;
