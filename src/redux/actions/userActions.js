e8ca06d7b899d99263c68b8ac42062863a4199e4
Romain <romain@romains-macbook-pro.home>
2019-09-29 01:09:17 +0200

    Profile section

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,4 +1,4 @@
-import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI, SET_UNAUTHENTICATED } from "../types";
+import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI, SET_UNAUTHENTICATED, LOADING_USER } from "../types";
 
 import axios from "axios";
 
@@ -49,6 +49,8 @@ export const logoutUser = () => dispatch => {
 
 //Get the user data
 export const getUserData = () => dispatch => {
+	dispatch({ type: LOADING_USER });
+
 	axios
 		.get("/user")
 		.then(res => {
